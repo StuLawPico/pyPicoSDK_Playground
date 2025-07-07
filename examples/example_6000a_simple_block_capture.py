65260fe7e1e39394d27d0a307268f310f7498dd2
JamesPicoTech <118216728+JamesPicoTech@users.noreply.github.com>
2025-07-07 15:01:46 +0100

    Merge simple block and siggen examples
    - Added SigGen to SigGen example and renamed to simple block
    - Removed SimpleBlock

--- examples/example_6000a_simple_block_capture.py
+++ examples/example_6000a_simple_block_capture.py
@@ -4,19 +4,21 @@ from matplotlib import pyplot as plt
 # Pico examples use inline argument values for clarity
 
 # Capture configuration
-SAMPLES = 50_000
+SAMPLES = 5_000
 
 # Initialise PicoScope 6000
 scope = psdk.ps6000a()
 scope.open_unit()
 
+# Setup siggen
+scope.set_siggen(frequency=1_000_000, pk2pk=0.8, wave_type=psdk.WAVEFORM.SINE)
+
 # Setup channels and trigger (inline arguments)
 scope.set_channel(channel=psdk.CHANNEL.A, range=psdk.RANGE.V1)
-scope.set_channel(channel=psdk.CHANNEL.B, range=psdk.RANGE.V1)
 scope.set_simple_trigger(channel=psdk.CHANNEL.A, threshold_mv=0)
 
 # Preferred: convert sample rate to timebase
-TIMEBASE = scope.sample_rate_to_timebase(50, psdk.SAMPLE_RATE.MSPS)
+TIMEBASE = scope.sample_rate_to_timebase(sample_rate=500, unit=psdk.SAMPLE_RATE.MSPS)
 # TIMEBASE = 2  # direct driver timebase
 # TIMEBASE = scope.interval_to_timebase(20E-9)
 
@@ -27,13 +29,10 @@ channel_buffer, time_axis = scope.run_simple_block_capture(TIMEBASE, SAMPLES)
 scope.close_unit()
 
 # Plot data to pyplot
-plt.plot(time_axis, channel_buffer[psdk.CHANNEL.A], label="Channel A")
-plt.plot(time_axis, channel_buffer[psdk.CHANNEL.B], label="Channel B")
+plt.plot(time_axis, channel_buffer[psdk.CHANNEL.A])
 
 # Add labels to pyplot
 plt.xlabel("Time (ns)")     
 plt.ylabel("Amplitude (mV)")
-plt.ylim(-500, 500)
-plt.legend()
 plt.grid(True)
 plt.show()
